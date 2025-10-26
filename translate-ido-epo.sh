409e7b002f3687f6b6c5439d0ed7e24965eb1c87
Mark <komapc@gmail.com>
2025-10-26 10:53:45 +0200

    chore: remove redundant shell scripts and clean up AUTHORS
    - Remove setup-ido-epo-function.sh (redundant with Apertium modes)
    - Remove translate-ido-epo.sh (redundant with Apertium modes)
    - Remove 'Apertium Community' from AUTHORS file
    - Standard Apertium commands (apertium ido-epo/epo-ido) work via modes.xml

--- translate-ido-epo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Simple wrapper for ido-epo translation
-# Usage: echo "text" | ./translate-ido-epo.sh
-
-cd "$(dirname "$0")"
-lt-proc ido-epo.automorf.bin | apertium-pretransfer | lt-proc -b ido-epo.autobil.bin | apertium-transfer -b apertium-ido-epo.ido-epo.t1x ido-epo.t1x.bin | lt-proc -g ido-epo.autogen.bin | lt-proc -p ido-epo.autopgen.bin
